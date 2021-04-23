9c97672c8cb4c63ddc34906d1ef25eab278354ab
bendenizdeniz <denizcanaltun0@gmail.com>
2021-04-23 17:17:02 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repo.GetList();
+        }
+
+        public void CategoryAddBL(Category category)
+        {
+            if (category.CategoryName == "" || category.CategoryName.Length <= 3 ||
+                category.CategoryDescription == "" || category.CategoryName.Length >= 51)
+            { 
+                    //hata mesajı
+            }
+            else
+            {
+                repo.Insert(category);
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> GetList();
+
+        void Insert(T item);
+
+        void Update(T item);
+
+        void Delete(T item);
+
+        List<T> List(Expression<Func<T, bool>> filter); //koşullu filtreleme
+    }
+}
